872939768920e2c0cc9be03ead732b9a569820b6
JohnHBenn <jhbenn39@harrisonburg.k12.va.us>
2019-09-19 09:51:20 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var score = 1; // Create a variable to store the score in
 
-var title1 = new Title('Dont touch the button'); // Create a title element
+var title = new Title('Dont touch the button'); // Create a title element
 var button = new Button('Button', btnPress);   // Create a button element
                  // Create a text element
 
@@ -10,25 +10,25 @@ changeTitle('Clicker Game');
 function btnPress() {
   score++; 
   if (score == 2){
-    title1.hide();
+    title.hide();
     button.hide();
-    var title2 = new Title("Dont touch the butt");
+    var title1 = new Title("Dont touch the butt");
     var button1 = new Button('Butt', btn1Press);
   }
 }
 function btn1Press() {
   score++;
   if (score == 3){
-    title2.hide();
-    var title3 = new Title("Why you touch ma butt");
+    title1.hide();
+    var title2 = new Title("Why you touch ma butt");
   }
 }
 function btn2Press() {
   score++;
   if (score == 4){
-    title3.hide();
+    title2.hide();
     button1.hide();
-    var title4 = new Title("This is sexual harrasment im calling the po po");
+    var title3 = new Title("This is sexual harrasment im calling the po po");
     var button2 = new Button("No don't call the po po", btn2Press);
   }
 }
