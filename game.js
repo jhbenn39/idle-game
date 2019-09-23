0577b279b261ddd49824410c7ea01e0a3710b78b
jhbenn39 <55092194+jhbenn39@users.noreply.github.com>
2019-09-23 13:16:51 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,16 +1,80 @@
-var scr1 = 0; // Create a variable to store the score in
+var scr = 0;
+var scr1 = 0;
+var scr2 = 0;
+var scr3 = 0;
+var scr4 = 0;
+var scr5 = 0;
+var scr6 = 0;
+var scr7 = 0;
+var scr8 = 0;
+var scr9 = 0;
+var scr10 = 0;
+var scr11 = 0;
+var scr12 = 0;
 
 var beginning = new Text('You are all alone in your little world with no civilization and you have to make a computer to get back to people.');
-var text1 = new Text('get some stone to start your journey')
-var text2 = new Text('Great! Now you should grab some sticks to make a stone pickaxe')
+var text = new Text('get some stone to start your journey')
+var text1 = new Text('Great! Now you should grab some sticks to make a stone pickaxe')
 var text2 = new Text('Now you can get stone more quickly')
+var text3 = new Text('You can now make a smelter')
+var text4 = new Text('Make a stone axe to get wood for your smelter')
+var text5 = new Text('You can now find iron')
+var text6 = new Text('upgrade your smelter to smelt your iron faster')
+var text7 = new Text("You find some clay but you can't get it because you need a shovel to get it")
+var text8 = new Text('Make a shovel')
+var text9 = new Text('you can now get clay')
+var text10 = new Text('you can now make iron tools to get materials efficiently')
+var text11 = new Text('')
 
 var button = new Button('Stone',btnprs);
 var button1 = new Button('Sticks',btn1prs);
 var button2 = new Button('Stone Pickaxe',btn3prs);
-
-
+var button = new Button('Smelter',btn4prs);
+var button = new Button('Stone axe',btn5prs);
+var button = new Button('Wood',btn6prs);
+var button = new Button('Stone shovel',btn7prs);
+var button = new Button('Clay',btn8prs);
+var button = new Button('Iron',btn9prs);
+var button = new Button('Iron pickaxe',btn10prs);
+var button = new Button('Iron axe',btn11prs);
+var button = new Button('Iron shovel',btn12prs);
 
 function btnprs(){
+  scr ++;
+}
+function btn1prs(){
   scr1 ++;
+}
+function btn2prs(){
+  scr2 ++;
+}
+function btn3prs(){
+  scr3 ++;
+}
+function btn4prs(){
+  scr4 ++;
+}
+function btn5prs(){
+  scr5 ++;
+}
+function btn6prs(){
+  scr6 ++;
+}
+function btn7prs(){
+  scr7 ++;
+}
+function btn8prs(){
+  scr8 ++;
+}
+function btn9prs(){
+scr9 ++;
+}
+function btn10prs(){
+  scr20 ++;
+}
+function btn11prs(){
+  scr11 ++;
+}
+function btn12prs(){
+  scr12 ++;
 }
