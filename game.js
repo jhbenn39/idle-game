aaf970c8c997e20b5878111b7c52479aa7bfa450
JohnHBenn <jhbenn39@harrisonburg.k12.va.us>
2019-09-24 09:08:27 -0400

    gfghg

--- game.js
+++ game.js
@@ -11,30 +11,21 @@ var scr9 = 0;
 var scr10 = 0;
 var scr11 = 0;
 
+
+
 var beginning = new Text('You are all alone in your little world with no civilization and you have to make a computer to get back to people.');
-var text = new Text('get some stone to start your journey')
-var text1 = new Text('Stone = ' + scr)
-var text2 = new Text('Great! Now you should grab some sticks to make a stone pickaxe')
-var text3 = new Text('Sticks = ' + scr1)
-var text4 = new Text('Stone Pickaxe = ' + scr2)
-var text5 = new Text('Now you can get stone more quickly')
-var text6 = new Text('You can now make a smelter')
-var text7 = new Text('Smelter upgrades = ' + scr3)
-var text8 = new Text('upgrade your smelter to smelt your iron faster')
-var text9 = new Text('Make a stone axe to get wood for your smelter')
-var text10 = new Text('Stone Axe = ' + scr4)
-var text11 = new Text('Wood = ' + scr5)
-var text12 = new Text('You can now find iron but you cant smelt it into anything')
-var text13 = new Text("You find some clay but you can't get it because you need a shovel to get it")
-var text14 = new Text('Make a shovel')
-var text15 = new Text('Stone Shovel = ' + scr6)
-var text16 = new Text('you can now get clay')
-var text17 = new Text('Clay = ' + scr7)
-var text18 = new Text('you can now make iron tools to get materials efficiently')
-var text19 = new Text('Iron = ' + scr8)
-var text20 = new Text('Pickaxe upgrade = ' + scr2)
-var text21 = new Text('Axe upgrade = ' + scr4)
-var text22 = new Text('Shovel upgrade = ' + scr6)
+var text = new Text('Get some stone to start your journey');
+var text1 = new Text('Great! Now you should grab some sticks to make a stone pickaxe');
+var text2 = new Text('Now you can get stone more quickly');
+var text3 = new Text('You can now make a smelter');
+var text4 = new Text('Upgrade your smelter to smelt your iron faster');
+var text5 = new Text('Make a stone axe to get wood for your smelter');
+var text6 = new Text('You can now find iron but you cant smelt it into anything');
+var text7 = new Text("You find some clay but you can't get it because you need a shovel to get it");
+var text8 = new Text('Make a stone shovel');
+var text9 = new Text('You can now get clay');
+var text10 = new Text('You can now make iron tools to get materials efficiently');
+
 
 
 var button = new Button('Stone',btnprs);
@@ -50,51 +41,113 @@ var button9 = new Button('Iron pickaxe',btn9prs);
 var button10 = new Button('Iron axe',btn10prs);
 var button11 = new Button('Iron shovel',btn11prs);
 
+
+
+var mat = new Text('Stone = ' + scr);
+var mat1 = new Text('Sticks = ' + scr1);
+var mat2 = new Text('Stone Pickaxe = 40 Stone + 20 Sticks');
+var mat3 = new Text('Smelter upgrades = ' + scr3);
+var mat4 = new Text('Stone Axe = ' + scr4);
+var mat5 = new Text('Wood = ' + scr5);
+var mat6 = new Text('Stone Shovel = ' + scr6);
+var mat7 = new Text('Clay = ' + scr7);
+var mat8 = new Text('Iron = ' + scr8);
+var mat9 = new Text('Pickaxe upgrade = ' + scr2);
+var mat10 = new Text('Axe upgrade = ' + scr4);
+var mat11 = new Text('Shovel upgrade = ' + scr6);
+
+
+
+var rowSection = new Section();
+  var txt = new Section();
+  var btns = new Section();
+  var mats = new Section();
+    rowSection.add(txt);
+    rowSection.add(btns);
+    rowSection.add(mats);
+      txt.add(text);
+      txt.add(text1);
+      txt.add(text2);
+      txt.add(text3);
+      txt.add(text4);
+      txt.add(text5);
+      txt.add(text6);
+      txt.add(text7);
+      txt.add(text8);
+      txt.add(text9);
+      txt.add(text10);
+      
+      btns.add(button);
+      btns.add(button1);
+      btns.add(button2);
+      btns.add(button3);
+      btns.add(button4);
+      btns.add(button5);
+      btns.add(button6);
+      btns.add(button7);
+      btns.add(button8);
+      btns.add(button9);
+      btns.add(button10);
+      btns.add(button11);
+      
+      mats.add(mat);
+      mats.add(mat1);
+      mats.add(mat2);
+      mats.add(mat3);
+      mats.add(mat4);
+      mats.add(mat5);
+      mats.add(mat6);
+      mats.add(mat7);
+      mats.add(mat8);
+      mats.add(mat9);
+      mats.add(mat10);
+      mats.add(mat11);
+
 function btnprs(){
   scr ++;
-  text1.edit('Stone = ' + scr);
+  mat.edit('Stone = ' + scr);
 }
 function btn1prs(){
   scr1 ++;
-  text3.edit('Sticks = ' + scr1);
+  mat1.edit('Sticks = ' + scr1);
 }
 function btn2prs(){
   scr2 ++;
-  text4.edit('Stone Pickaxe = ' + scr2);
+  mat2.hide();
 }
 function btn3prs(){
   scr3 ++;
-  text7.edit('Smelter upgrades = ' + scr3);
+  mat3.edit('Smelter upgrades = ' + scr3);
 }
 function btn4prs(){
   scr4 ++;
-  text10.edit('Stone Axe = ' + scr4);
+  mat4.hide();
 }
 function btn5prs(){
   scr5 ++;
-  text11.edit('Wood = ' + scr5);
+  mat5.edit('Wood = ' + scr5);
 }
 function btn6prs(){
   scr6 ++;
-  text15.edit('Stone Shovel = ' + scr6);
+  mat6.hide();
 }
 function btn7prs(){
   scr7 ++;
-  text17.edit('Clay = ' + scr7);
+  mat7.edit('Clay = ' + scr7);
 }
 function btn8prs(){
   scr8 ++;
-  text19.edit('Iron = ' + scr8);
+  mat8.edit('Iron = ' + scr8);
 }
 function btn9prs(){
   scr9 ++;
-  text20.edit('Iron Pickaxe = ' + scr9);
+  mat9.hide();
 }
 function btn10prs(){
   scr10 ++;
-  text21.edit('Iron Axe = ' + scr10);
+  mat10.hide();
 }
 function btn11prs(){
   scr11 ++;
-  text22.edit('Iron Shovel = ' + scr11);
+  mat11.hide();
 }
